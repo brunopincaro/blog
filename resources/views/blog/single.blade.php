90329e1dc4f26e40f3f7bd70cf16c08ad6f914dc
brunopincaro <brunopraia@gmail.com>
2017-09-16 23:03:14 +0200

    Post's category
    Pull the post's category and show it

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -7,6 +7,16 @@
 		<div class="col-md-8 col-md-offset-2">
 			<h1>{{ $post->title }}</h1>
 			<p>{{ $post->body }}</p>
+			<hr>
+			<p>Posted in: {{ $post->category->name }}</p>
+			<?
+				/*
+				because there's a relationship between the tables posts and categories, we can access the information on the other table this way
+
+				$post is an object of the Post model
+				category property is the method Post's method category() that initiates the relationship 
+				*/
+			?>
 		</div>
 	</div>
 @stop
